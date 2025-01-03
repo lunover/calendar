a70c782ffccb32f23226b8983053d4889a7f4b03
rajushrestha <raju@lunover.com>
2025-01-03 18:02:47 +0545

    Add Vercel Analytics integration
    - Included @vercel/analytics package in package.json and pnpm-lock.yaml.
    - Integrated Analytics component into the RootLayout of the application for enhanced tracking and insights.
    
    This addition aims to improve user engagement analysis and application performance monitoring.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
@@ -45,6 +46,8 @@ export default function RootLayout({
             </footer>
           </div>
         </ThemeProvider>
+
+        <Analytics />
       </body>
     </html>
   );
